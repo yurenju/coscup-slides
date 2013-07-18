371bf6e80986cc8bed9374ccd6802080d0289591
Yuren Ju <yurenju@gmail.com>
2013-07-18 23:51:57 +0800

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,38 @@
+
+'use strict';
+// use app here
+window.addEventListener('load', function() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'scripts/faq.json', true);
+    xhr.responseType = 'json';
+    xhr.send();
+    var question = document.querySelector('#question');
+    var answer = document.querySelector('#answer');
+    var author = document.querySelector('#author');
+    var reference = document.querySelector('#reference');
+    var container = document.querySelector('#container');
+
+    function update(item) {
+        container.classList.remove('up');
+        question.textContent = item['question'];
+        answer.textContent = item['answer'];
+        author.textContent = item['provider'];
+        reference.textContent = item['reference'];
+        answer.classList.add('show');
+    }
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            var res = xhr.response;
+            var item = res[Math.floor(Math.random() * res.length)];
+            answer.classList.remove('show');
+            update(item);
+            window.setInterval(function() {
+                item = res[Math.floor(Math.random() * res.length)];
+                answer.classList.remove('show');
+                container.classList.add('up');
+                window.setTimeout(update.bind(null, item), 1000);
+            }, 20000);
+        }
+    };
+})
